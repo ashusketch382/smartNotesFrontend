e9f965a03ef036e70e1d52a87f16702318a2492f
ashusketch382 <ashutosh382singh@gmail.com>
2025-05-02 23:52:36 +0530

    corrected sign in functionality of sign in page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,28 +1,30 @@
 "use client"
 import { useState } from 'react';
-//import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import axios from 'axios';
 
 export default function() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
-//    const router = useRouter();
+    const router = useRouter();
 
     const handleSubmit = async (e: React.FormEvent) => {
+        console.log('submitted');
         e.preventDefault();
         try {
-            const res = await axios.post('http://localhost:5000/api/auth/login', { username: "ash", email, password });
+            const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+            console.log(res.data);
             localStorage.setItem('token', res.data.token);
-//            router.push('/dashboard');
+            router.push('/dashboard');
         } catch (err) {
-            setError('Invalid credentials');
+            setError('Bad Credentials');
         }
     };
 
     return <div className="h-screen flex justify-center flex-col">
         <form onSubmit={handleSubmit} className="flex justify-center">
-        <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
+        <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
                 <div>
                     <div className="px-10">
                         <div className="text-3xl font-extrabold">
@@ -33,10 +35,10 @@ export default function() {
                     <div className="pt-2">
                         <LabelledInput label="Username" type={"email"} placeholder="ashutosh382singh@gmail.com" onChange={(e) => {setEmail(e.target.value); }} />
                         <LabelledInput label="Password" type={"password"} placeholder="123456" onChange={(e) => {setPassword(e.target.value); }} />
-                        <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign In</button>
+                        <button type="submit" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign In</button>
                     </div>
                 </div>
-            </a>
+            </div>
         </form>
     </div>
 }
